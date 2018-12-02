b4418ee8807365aff59beaca2a0b3f7eb89ebade
ShaneMarusczak <marusczaks@gmail.com>
2018-12-02 09:07:37 -0500

    code clean up

--- src/main/java/org/wecanreadit/LibrarianPopulator.java
+++ src/main/java/org/wecanreadit/LibrarianPopulator.java
@@ -13,17 +13,7 @@ public class LibrarianPopulator implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Librarian mike = new Librarian("Mike", "Myers", "Beachwood", "waynesworld.com", "waynesworld1", "password",
-				"action");
-		Librarian angie = new Librarian("Angie", "Smith", "Heights", "smithtown.com", "smittylady5", "password5",
-				"biography");
-
-		Librarian ashley = new Librarian("Ashley", "Stanley", "South Euclid-Lyndhurst", "sheslays.com", "ash321",
-				"password91", "science fiction");
-
-		mike = librarianRepo.save(mike);
-		angie = librarianRepo.save(angie);
-		ashley = librarianRepo.save(ashley);
+		
 
 	}
 }
